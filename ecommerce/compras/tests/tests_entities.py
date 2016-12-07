254542ee588419aab42129147e334414bda349c6
Wowip <arturoinosencio@gmail.com>
2016-12-07 12:43:28 -0600

    Trayendo a la compu de Arturo

--- ecommerce/compras/tests/tests_entities.py
+++ ecommerce/compras/tests/tests_entities.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from compras.models import Carrito
+import json
 
 # Create your tests here.
 class TestProduct(TestCase):
@@ -7,10 +8,13 @@ class TestProduct(TestCase):
         lstProductos=[[10,9999.99],[20,9999.92],[20,9999.99]]
         carrito = Carrito.objects.create(
             idCliente=1,
-            listaProductos=lstProductos,
+            listaProductos=json.dumps(lstProductos),
             total=99999.99
         )
 
 
-        objCarrito = Carrito.objects.get(pk = carrito.id)
-        self.assertEquals(objCarrito.listaProductos.to_python(),lstProductos);
+        
+        objCarrito =Carrito.objects.get(pk = carrito.id)
+        listaProducto = json.loads(objCarrito.listaProductos)
+        self.assertEquals(listaProducto,lstProductos)
+
