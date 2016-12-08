9679398f4cad122e917483fed4d907173b823ee5
Angel gustavo <agustavo79@gmail.com>
2016-12-08 09:55:25 -0800

    Se agrega la estructira de la clase Carrito y OrdenCompra en la capa de business-logic

--- ecommerce/compras/tests/tests_entities.py
+++ ecommerce/compras/tests/tests_entities.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from compras.models import Carrito
+from compras.models import Carrito, Producto, OrdenCompra
 import json
 
 # Create your tests here.
@@ -18,3 +18,29 @@ class TestProduct(TestCase):
         objCarrito = Carrito.objects.get(pk=carrito.id)
         listaProducto = json.loads(objCarrito.listaProductos)
         self.assertEquals(listaProducto, lstProductos)
+
+    def test_saveProducto(self):
+        producto = Producto.objects.create(
+            nombre  = "Camisa",
+            descripcion = "Camisa blanca tipo polo tamaño regular",
+            marca = "Polo",
+            precio = 540.50
+            
+        )
+
+        objProducto = Producto.objects.get(pk=producto.id)
+
+        self.assertEquals(producto, objProducto)
+
+    def test_OrdenCompra(self):
+        lstProductos = [[10, 9999.99], [20, 9999.92], [20, 9999.99]]
+        ordenCompra = OrdenCompra.objects.create(
+            idCliente = 1,
+            listaProductosOrden = json.dumps(lstProductos),
+            status = 0            
+        )
+
+        objOrdenCompras = OrdenCompra.objects.get(pk=ordenCompra.id)
+        listaProductosOrden = json.loads(objOrdenCompras.listaProductosOrden)
+        
+        self.assertEquals(lstProductos, listaProductosOrden)
