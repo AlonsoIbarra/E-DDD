2fd7c51ded87e1f1fc69ecc6704bca62416d7023
Wowip <arturoinosencio@gmail.com>
2016-12-08 09:35:40 -0600

    Añadiendo carpeta de templates y cambiando views.py por application_service.py

--- ecommerce/compras/entities.py
+++ ecommerce/compras/entities.py
@@ -1,10 +1,11 @@
 from django.db import models
-import ast
 
 # Create your models here.
 
+
 class Entity (models.Model):
     pass
+
 
 class Producto (Entity):
     idproducto = models.AutoField(primary_key=True)
@@ -13,6 +14,7 @@ class Producto (Entity):
     marca = models.CharField(max_length=30)
     precio = models.DecimalField(decimal_places=2, max_digits=8)
 
+
 class Carrito (Entity):
     idCarrito = models.AutoField(primary_key=True)
     idCliente = models.IntegerField()
@@ -20,15 +22,15 @@ class Carrito (Entity):
     fechaCarrito = models.DateField(auto_now=True)
     total = models.DecimalField(decimal_places=2, max_digits=8)
 
+
 class OrdenCompra (Entity):
     idOrdenCompra = models.AutoField(primary_key=True)
     fechaCompra = models.DateField(auto_now_add=True)
     idCliente = models.IntegerField()
     listaProductosOrden = models.TextField()
     STATUS_CHOICES = (
-	(0, "Pagada"),
-	(1, "Pendiente"),
-	(2, "Cancelada"),
+        (0, "Pagada"),
+        (1, "Pendiente"),
+        (2, "Cancelada"),
     )
-    status = models.IntegerField(choices = STATUS_CHOICES)
-
+    status = models.IntegerField(choices=STATUS_CHOICES)
--- ecommerce/compras/tests/tests_entities.py
+++ ecommerce/compras/tests/tests_entities.py
@@ -3,18 +3,18 @@ from compras.models import Carrito
 import json
 
 # Create your tests here.
+
+
 class TestProduct(TestCase):
+
     def test_saveListProduct(self):
-        lstProductos=[[10,9999.99],[20,9999.92],[20,9999.99]]
+        lstProductos = [[10, 9999.99], [20, 9999.92], [20, 9999.99]]
         carrito = Carrito.objects.create(
             idCliente=1,
             listaProductos=json.dumps(lstProductos),
             total=99999.99
         )
 
-
-        
-        objCarrito =Carrito.objects.get(pk = carrito.id)
+        objCarrito = Carrito.objects.get(pk=carrito.id)
         listaProducto = json.loads(objCarrito.listaProductos)
-        self.assertEquals(listaProducto,lstProductos)
-
+        self.assertEquals(listaProducto, lstProductos)
