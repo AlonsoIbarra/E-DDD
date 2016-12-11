f954bbd88508e0195bff38e23b02c5ad832973aa
Angel gustavo <agustavo79@gmail.com>
2016-12-11 11:26:31 -0800

    Creacion de clase ProductCatalog en el modulo business_logic

--- ecommerce/compras/business_logic.py
+++ ecommerce/compras/business_logic.py
@@ -1,6 +1,7 @@
 from compras import models
 import json
 
+
 class Carrito():
     def __init__(self, pIdCliente):
         if models.Carrito.objects.filter(idCliente = pIdCliente).exists():
@@ -31,17 +32,43 @@ class Carrito():
         return sum([cantidad * precio for id, cantidad, precio in listaProductos])
 
 
-class OrdenCompra():
+class PurchaseOrder():
+
     def __init__(self):
-        pass
-    
-    def adquirirCarrito(self, pCarrito):
-        pass
-    
-    def mostrarDetalle(self):
-        return self.OrdenCompra
+        self.ordenCompra = models.OrdenCompra()
+
+    def buyArticles(self, pCarrito):
+        self.ordenCompra = models.ordenCompra.objects.create(
+            idCliente=1,
+            listaProductosOrden=pCarrito.listaProductos,
+            status=1,
+        )
+
+    def getDetails(self):
+        return self.ordenCompra
 
     @staticmethod
     def find(order_id):
         return models.OrdenCompra.objects.get(id=order_id)
 
+
+class ProductCatalog():
+    def __init__(self):
+        pass
+
+    def find(self, pidProduct):
+        return models.Producto.objects.get(pk=pidProduct)
+
+    def getAll(self):
+        return models.Producto.objects.get()
+
+
+class PurchaseOrderList():
+    def __init__(self):
+        pass
+
+    def findById(self, pidPurchaseOrder):
+        return models.OrdenCompra.objects.get(pk=pidPurchaseOrder)
+    
+    def getAll(self):
+        return models.OrdenCompra.objects.get()
