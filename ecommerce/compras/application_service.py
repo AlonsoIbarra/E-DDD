f4da90c003df4026350469d5229483d5375f5880
César Antáres <zzantares@gmail.com>
2016-12-12 19:30:08 -0600

    Lista los detalles de la orden conforme a las pruebas
    Aún falta agregarle estilos, el boton de pagar y el calculo del
    total (se agregarán más pruebas para eso).

--- ecommerce/compras/application_service.py
+++ ecommerce/compras/application_service.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from compras.models import Producto 
-# Create your views here.
+from django.http import HttpResponseNotFound
+from django.core.exceptions import ObjectDoesNotExist
+from compras.models import Producto
+from compras.business_logic import OrdenCompra
+
 
 def order_detail(request, id):
-    return render(request, 'order_detail.html')
+    try:
+        order = OrdenCompra.find(id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound('<h1>Orden de compra no existe!</h1>')
+
+    return render(request, 'order_detail.html', {'order': order})
+
 
 def ver_detalles(request, idProducto):
 #	detalleProducto = Producto.objects.get(id=idProducto)
@@ -14,4 +23,3 @@ def ver_detalles(request, idProducto):
         precio = 540.50
     )
 	return render(request,'detalles_producto.html',{'productoDetalle' : producto})
-	
